06b9d34cb81a96527bafc225be4b8ae2a6ceb883
Felix Angell <felixangell1024@gmail.com>
2016-02-14 17:54:56 +0000

    more string functions

--- strings/functions.go
+++ strings/functions.go
@@ -1,9 +1,16 @@
 package strings
 
-import "github.com/raoulvdberge/risp/runtime"
+import (
+	"github.com/raoulvdberge/risp/runtime"
+	"strings"
+)
 
 var Symbols = runtime.Symtab{
-	"substr": runtime.NewSymbol(runtime.NewFunctionValue(runtime.NewBuiltinFunction(stringsSubstr, "substr"))),
+	"substr":  runtime.NewSymbol(runtime.NewFunctionValue(runtime.NewBuiltinFunction(stringsSubstr, "substr"))),
+	"trim":    runtime.NewSymbol(runtime.NewFunctionValue(runtime.NewBuiltinFunction(stringsTrim, "trim"))),
+	"join":    runtime.NewSymbol(runtime.NewFunctionValue(runtime.NewBuiltinFunction(stringsJoin, "join"))),
+	"rune-at": runtime.NewSymbol(runtime.NewFunctionValue(runtime.NewBuiltinFunction(stringsRuneAt, "rune-at"))),
+	"length":  runtime.NewSymbol(runtime.NewFunctionValue(runtime.NewBuiltinFunction(stringsLength, "length"))),
 }
 
 func stringsSubstr(context *runtime.FunctionCallContext) (*runtime.Value, error) {
@@ -26,4 +33,42 @@ func stringsSubstr(context *runtime.FunctionCallContext) (*runtime.Value, error)
 	result := source[start:end]
 
 	return runtime.NewStringValue(result), nil
+}
+
+func stringsTrim(context *runtime.FunctionCallContext) (*runtime.Value, error) {
+	if err := runtime.ValidateArguments(context, runtime.StringValue, runtime.StringValue); err != nil {
+		return nil, err
+	}
+
+	source := context.Args[0].Str
+	cutset := context.Args[1].Str
+	return runtime.NewStringValue(strings.Trim(source, cutset)), nil
+}
+
+func stringsJoin(context *runtime.FunctionCallContext) (*runtime.Value, error) {
+	if err := runtime.ValidateArguments(context, runtime.StringValue, runtime.StringValue); err != nil {
+		return nil, err
+	}
+
+	first := context.Args[0].Str
+	second := context.Args[1].Str
+	return runtime.NewStringValue(first + second), nil
+}
+
+func stringsRuneAt(context *runtime.FunctionCallContext) (*runtime.Value, error) {
+	if err := runtime.ValidateArguments(context, runtime.StringValue, runtime.NumberValue); err != nil {
+		return nil, err
+	}
+
+	source := context.Args[0].Str
+	index := context.Args[1].NumberToInt64()
+	return runtime.NewStringValue(string([]rune(source)[index])), nil
+}
+
+func stringsLength(context *runtime.FunctionCallContext) (*runtime.Value, error) {
+	if err := runtime.ValidateArguments(context, runtime.StringValue); err != nil {
+		return nil, err
+	}
+
+	return runtime.NewNumberValueFromInt64(int64(len(context.Args[0].Str))), nil
 }
