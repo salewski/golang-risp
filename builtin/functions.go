8ecf0f64bbc22e6922bc25a4203bc98eb7731569
U-DESKTOP-UR4LC23\Raoul <raoulvdberge@gmail.com>
2016-02-15 20:57:01 +0100

    fix test + cleanup assertion code

--- builtin/functions.go
+++ builtin/functions.go
@@ -239,26 +239,23 @@ func builtinCat(context *runtime.FunctionCallContext) (*runtime.Value, error) {
 
 func builtinAssert(context *runtime.FunctionCallContext) (*runtime.Value, error) {
 	optionalError := runtime.ValidateArguments(context, runtime.BooleanValue)
+
 	if optionalError != nil {
 		err := runtime.ValidateArguments(context, runtime.BooleanValue, runtime.StringValue)
+
 		if err != nil {
 			return nil, err
 		}
 	}
 
-	// todo improve errors to show the line in the source code
-	// we can get this from the context but there doesn't seem to
-	// be an easy way to get to the source of the file the function
-	// belongs to
+	message := "assertion failed"
 
-	var assertionFailedError string
 	if len(context.Args) == 2 {
-		assertionFailedError = ":" + context.Args[1].Str
+		message += ": " + context.Args[1].Str
 	}
 
-	assertion := context.Args[0].Boolean
-	if !assertion {
-		return nil, runtime.NewRuntimeError(context.Pos, "assertion failed "+assertionFailedError)
+	if !context.Args[0].Boolean {
+		return nil, runtime.NewRuntimeError(context.Pos, message)
 	}
 
 	return runtime.Nil, nil
